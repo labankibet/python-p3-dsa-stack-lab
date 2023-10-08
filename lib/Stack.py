8e3f159c5a09f76633276acb6e35e808cf20e787
labankibet <laban.kibet@moringaschool.com>
2023-10-08 21:18:26 +0300

    done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,42 @@
 class Stack:
-
+    
     def __init__(self, items = [], limit = 100):
-        pass
-
+        self.items = items
+        self.limit = limit
+    
     def isEmpty(self):
-        pass
-
+        return len(self.items) == 0
+    
+    # adding an item to the top of the stack
     def push(self, item):
-        pass
-
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise OverflowError("Stack is full")
+        
+    # removing an item from the top of the stack
     def pop(self):
-        pass
-
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty")
+    
+    # getting the top item without removing it
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack is empty")
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
-
+        return len(self.items) == self.limit
+    
+    # returning the index of the first occurrence of the target element in the stack
     def search(self, target):
-        pass
+        for i in range(len(self.items) - 1, -1, -1):
+            if self.items[i] == target:
+                return i
+        return -1
